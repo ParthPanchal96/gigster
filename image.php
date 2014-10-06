c8a33b47ffd60c8a74af4e8934c36c0d42e363cb
ashishScripter <aashish.singhal@fountaintechies.com>
2014-10-06 15:58:15 +0530

    image error checking  2.7

--- image.php
+++ image.php
@@ -79,7 +79,7 @@ define('DOCUMENT_ROOT',$_SERVER['DOCUMENT_ROOT']."/");
 }
 // Images must be local files, so for convenience we strip the domain if it's there
 $image			= preg_replace('/^(s?f|ht)tps?:\/\/[^\/]+/i', '', (string) $_GET['image']);
-
+print_r($image); die();
 // For security, directories cannot contain ':', images cannot contain '..' or '<', and
 // images must start with '/'
 if ($image{0} != '/' || strpos(dirname($image), ':') || preg_match('/(\.\.|<|>)/', $image))
